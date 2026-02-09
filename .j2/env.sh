1932e55c9d16d44a0300d8cfbdf5d63c834da22a
Jay Two <jaytwo@jaytwo.kr>
2026-02-09 20:52:11 +0900

    rename bash to sh

---
+++ .j2/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# current /.j2 directory
+# export J2_ROOT="$PWD/.j2"
+export J2_ROOT="$PWD"
+
+if [ ! -d "$J2_ROOT" ]; then
+    echo "[Warning] .j2 directory not found in the current path."
+fi
+
+export PATH="$J2_ROOT/bin:$PATH"
+export LD_LIBRARY_PATH="$J2_ROOT/lib:$LD_LIBRARY_PATH"
+
+echo "J2 Environment Activated."
+echo "J2_ROOT is set to: $J2_ROOT"
+
