668112e1463a602f5e768a2c26ea3cce95139004
Jay Two <jaytwo@jaytwo.kr>
2026-02-12 18:33:34 +0900

    pigar cmd

---
+++ .j2/bin/pigar.cmd
@@ -0,0 +1,9 @@
+
+:: install pigar if not already installed
+:: pip install pigar 
+
+:: generate requirements.txt file
+pigar -p requirements.txt -P . -o ">="
+:: -p : specify the output file name
+:: -P : specify the project directory
+:: -o : specify the version operator
